34b29b7dd8f1724776b84432cbde6e7e039b2a9d
ShubhamKulkarni14 <shubhuk14@gmail.com>
2020-11-26 19:27:36 +0530

    Added :Entering User Last Name

--- UserRegistration.sh
+++ UserRegistration.sh
@@ -1,12 +1,35 @@
 #!/bin/bash -x
 
-read -p "Enter First name :" fname
+function firstName()
+{
+	read -p "Enter First name :" fname
 
-fnamePattern="^[A-Z]+[a-z]{2,}$"
+	fnamePattern="^[A-Z][a-zA-Z]{2,}$"
 
-if [[ $fname =~ $fnamePattern ]]
-then
-        echo "First Name is valid"
-else
-        echo "First Name is invalid"
-fi
+	if [[ $fname =~ $fnamePattern ]]
+	then
+        	echo "First Name is valid"
+	else
+        	echo "First Name is invalid"
+	fi
+
+}
+
+function lastName()
+{
+	read -p "Enter Last name :" lname
+
+	lnamePattern="^[A-Z][a-zA-Z]{2,}$"
+
+	if [[ $lname =~ $lnamePattern ]]
+	then
+        	echo "last Name is valid"
+	else
+	        echo "last Name is invalid"
+	fi
+
+}
+
+
+firstName
+lastName
